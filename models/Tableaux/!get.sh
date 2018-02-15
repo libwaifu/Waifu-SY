3203183eff9c18af46fcc27839668a40da92c61f
GalAster <280597828@qq.com>
2018-02-15 20:52:00 +0800

    架构::Submodules init

---
+++ models/Tableaux/!get.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm raw/* -r && cd raw &&
+git submodule add "https://github.com/chinese-poetry/chinese-poetry-zhCN"
---
+++ tools/sm_update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# 子模块更新
+cd .. && git submodule foreach git pull
